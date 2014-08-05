fef4c275b7afb428d81bcc267b6c68560edc9649
Zack Fisch <zack@zfisch.com>
2014-08-05 18:51:18 -0400

    Getting basic crud up and running

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,22 @@
 class EventsController < ApplicationController
+
+	def index
+		@events = Event.all
+	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		if @event.save
+			redirect_to events_path
+		end
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:name, :date, :description)
+	end
 end
